653d8752aa7bec37077dd3d0f3fb2c9eb152279a
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2024-09-26 18:44:15 -0300

    Update books.py

--- Projeto Books to Scrape/books.py
+++ Projeto Books to Scrape/books.py
@@ -6,24 +6,28 @@ class BooksSpider(scrapy.Spider):
     name = "books"
     start_urls = ['http://books.toscrape.com/']
 
+    # Inicializamos uma lista para acumular os dados de todos os livros de todas as páginas
+    books_list = []
+
     def parse(self, response):
-        books_list = []
+        # Extrai os dados dos livros da página atual
         for book in response.css('article.product_pod'):
             book_data = {
                 'title': book.css('h3 a::attr(title)').get(),
                 'price': book.css('p.price_color::text').get(),
                 'availability': book.css('p.instock.availability::text').re_first('(\S+\s\S+)'),
             }
-            books_list.append(book_data)
-        
-        # Salva os dados dos livros em um arquivo JSON
-        with open('books.json', 'w') as f:
-            json.dump(books_list, f, indent=4)
+            self.books_list.append(book_data)
 
         # Caso haja próxima página
         next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None:
+            # Segue para a próxima página
             yield response.follow(next_page, self.parse)
+        else:
+            # Quando não houver mais páginas, salva os dados acumulados em um arquivo JSON
+            with open('books.json', 'w', encoding='utf-8') as f:
+                json.dump(self.books_list, f, ensure_ascii=False, indent=4)
 
 # Executa o spider
 process = CrawlerProcess()
