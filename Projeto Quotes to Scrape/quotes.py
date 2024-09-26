6e5cef97d522bf781f27ea5e115f719b169d4e52
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2024-09-26 18:39:27 -0300

    Update quotes.py

--- Projeto Quotes to Scrape/quotes.py
+++ Projeto Quotes to Scrape/quotes.py
@@ -6,23 +6,28 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = ['http://quotes.toscrape.com/']
 
+    # Inicializamos uma lista para acumular as citações de todas as páginas
+    quotes_list = []
+
     def parse(self, response):
-        quotes_list = []
+        # Extrai as citações da página atual
         for quote in response.css('div.quote'):
             quote_data = {
-                'text': quote.css('span.text::text').get(),
+                'text': quote.css('span.text::text').get().strip(),
                 'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
             }
-            quotes_list.append(quote_data)
-        
-        # Salva as citações em um arquivo JSON
-        with open('quotes.json', 'w') as f:
-            json.dump(quotes_list, f, indent=4)
+            self.quotes_list.append(quote_data)
 
         # Caso haja próxima página
         next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None:
+            # Segue para a próxima página
             yield response.follow(next_page, self.parse)
+        else:
+            # Quando não há mais páginas, salva os dados acumulados em um arquivo JSON
+            with open('quotes.json', 'w', encoding='utf-8') as f:
+                json.dump(self.quotes_list, f, ensure_ascii=False, indent=4)
 
 # Executa o spider
 process = CrawlerProcess()
