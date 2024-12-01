71f01b392df00ccfaf46dbbbb38bd1643606ca0e
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2024-12-01 18:50:50 -0300

    Delete PROJETOS/Projeto Simulador de AFs directory

--- PROJETOS/Projeto Simulador de AFs/estado.py
+++
@@ -1,8 +0,0 @@
-class Estado:
-    def __init__(self, nome, is_final=False):
-        self.nome = nome
-        self.is_final = is_final
-        self.transicoes = {}
-
-    def adicionar_transicao(self, simbolo, destino):
-        self.transicoes.setdefault(simbolo, []).append(destino)
