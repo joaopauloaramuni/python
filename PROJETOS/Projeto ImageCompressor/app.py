1fbc8cd0dcb0d1db42a6c165460f8effc1862502
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2025-02-08 09:41:48 -0300

    Update app.py

--- PROJETOS/Projeto ImageCompressor/app.py
+++ PROJETOS/Projeto ImageCompressor/app.py
@@ -2,7 +2,7 @@ from PIL import Image
 import os
 
 
-# Função para comprimir a imagem
+# Função para comprimir a imagem JPEG
 def comprimir_imagem(caminho_imagem, qualidade):
     try:
         img = Image.open(caminho_imagem)
@@ -16,11 +16,28 @@ def comprimir_imagem(caminho_imagem, qualidade):
         return f"Erro: {e}"
 
 
+# Função para comprimir a imagem PNG
+def comprimir_imagem_png(caminho_imagem):
+    try:
+        img = Image.open(caminho_imagem)
+        img = img.convert("P", palette=Image.ADAPTIVE, colors=128)  # Reduz para 128 cores
+
+        caminho_comprimido = os.path.join(
+            os.path.dirname(caminho_imagem),
+            "comprimido_" + os.path.basename(caminho_imagem),
+        )
+        img.save(caminho_comprimido, "PNG", optimize=True)
+        return caminho_comprimido
+    except Exception as e:
+        return f"Erro: {e}"
+
+
 # Função principal
 def main():
     caminho_imagem = "imgs/image.jpg"
-    qualidade = 50  # Definindo a qualidade para 50 (em vez de 0,5)
+    qualidade = 50  # Definindo a qualidade para 50
     resultado = comprimir_imagem(caminho_imagem, qualidade)
+    # resultado = comprimir_imagem_png(caminho_imagem)
     print(f"Imagem comprimida salva em: {resultado}")
 
 
